636d2c5dd7f9fe70cd13e52f4a868cdafbf491eb
shabaz2020 <shabaz8010@gmail.com>
2020-12-08 14:32:17 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    num_1 = Kernel.rand(1...20)
-    num_2 = Kernel.rand(1...20)
-    num_3 = Kernel.rand(1...20)
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
 
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
@@ -17,7 +17,6 @@ class Application
       resp.write "You Lose"
     end
 
-    
     resp.finish
   end
 
